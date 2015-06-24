56afda75d612e0487558a74a47b22bc05b237fcd
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-06-24 18:04:39 +0200

    Translation doesn't create index on (source, locale) by default

--- src/groovy/com/ticketbis/groobalize/Translation.groovy
+++ src/groovy/com/ticketbis/groobalize/Translation.groovy
@@ -1,19 +1,9 @@
 package com.ticketbis.groobalize
 
-import groovy.transform.TypeCheckingMode
-import grails.compiler.GrailsCompileStatic
+import groovy.transform.CompileStatic
 
-@GrailsCompileStatic
+@CompileStatic
 abstract class Translation<T> {
     Locale locale
     T source
-
-    @GrailsCompileStatic(TypeCheckingMode.SKIP)
-    static void translationConstraints(delegate) {
-        delegate.locale(unique: ['source'])
-    }
-
-    static constraints = {
-        Translation.translationConstraints(delegate)
-    }
 }
