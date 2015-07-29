765120bc4d943c5c8de59a6b200da45a3a34881c
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-07-29 10:53:29 +0200

    Test project updated for testing per-field fallbacks

--- test/projects/demo/grails-app/domain/demo/BookTranslation.groovy
+++ test/projects/demo/grails-app/domain/demo/BookTranslation.groovy
@@ -4,8 +4,12 @@ import com.ticketbis.groobalize.Translation
 import com.ticketbis.groobalize.ast.Field
 
 class BookTranslation extends Translation<Book> {
+    @Field(inherit=false)
     String title
 
-    @Field(inherit=false)
-    String synopsis
+    String synopsis = null
+
+    static constraints = {
+        synopsis(nullable: true)
+    }
 }
