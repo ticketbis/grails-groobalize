37af7be6d0eff5c7e634c377a6dbbc890f650ed8
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-07-29 11:50:34 +0200

    Added more integration test cases

--- test/projects/demo/test/integration/test/TranslationResolutionIntegrationSpec.groovy
+++ test/projects/demo/test/integration/test/TranslationResolutionIntegrationSpec.groovy
@@ -2,6 +2,7 @@ package test
 
 import demo.*
 import org.springframework.context.i18n.LocaleContextHolder as LCH
+import com.ticketbis.groobalize.WithFallbackLocaleContext
 import grails.test.spock.IntegrationSpec
 
 class TranslationResolutionIntegrationSpec extends IntegrationSpec {
@@ -12,7 +13,7 @@ class TranslationResolutionIntegrationSpec extends IntegrationSpec {
         book = new Book(author: "Dummy author", releaseDate: new Date())
 
         book.addToTranslations(title: "english title", synopsis: "an english synopsis", locale: new Locale('en'))
-        book.addToTranslations(title: "american english title", synopsis: "an american english synopsis", locale: new Locale('en', 'US'))
+        book.addToTranslations(title: "american english title", synopsis: null, locale: new Locale('en', 'US'))
         book.addToTranslations(title: "american pirate english title", synopsis: null, locale: new Locale('en', 'US', 'pirate'))
         book.addToTranslations(title: "titulo en castellano", synopsis: "sinopsis en castellano", locale: new Locale('es'))
         book.addToTranslations(title: "titulo en argentino", synopsis: "sinopsis en argentino", locale: new Locale('es', 'AR'))
@@ -25,11 +26,32 @@ class TranslationResolutionIntegrationSpec extends IntegrationSpec {
         book.delete()
     }
 
-    void "test basic locales fallback"() {
+    void "test simple locales fallback"() {
     given:
         LCH.locale = new Locale('es', 'ES')
     expect:
         book.title == "titulo en español"
         book.synopsis == "sinopsis en castellano"
     }
+
+    void "test locales fallback"() {
+    given:
+        LCH.locale = new Locale('en', 'US', 'pirate')
+    expect:
+        book.title == "american pirate english title"
+        book.synopsis == "an english synopsis"
+    }
+
+    void "test custom fallbacks"() {
+    given:
+        LCH.localeContext = new WithFallbackLocaleContext([
+            new Locale('es', 'UY'),
+            new Locale('es', 'ES'),
+            new Locale('en', 'US'),
+            new Locale('en')])
+    expect:
+        book.title == null // Name has no inheritance
+        book.synopsis == "an english synopsis"
+    }
+
 }
