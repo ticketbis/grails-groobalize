b7d3fda675ea6866eebb9e335dc780388e1975ca
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-08-31 10:25:27 +0200

    Added ROOT Locale to getLocaleTree

--- src/groovy/com/ticketbis/groobalize/GroobalizeHelper.groovy
+++ src/groovy/com/ticketbis/groobalize/GroobalizeHelper.groovy
@@ -6,6 +6,10 @@ import org.springframework.context.i18n.LocaleContext
 
 @CompileStatic
 class GroobalizeHelper {
+
+    // ToDo Locale.ROOT should be soported
+    static final Locale ROOT_LOCALE = null
+
     static def getField(Collection<Translation> translations,
             String property,
             boolean inherit = true,
@@ -31,7 +35,7 @@ class GroobalizeHelper {
     }
 
     private static List<Locale> getLocaleTree(Locale locale) {
-        if (!locale) return []
+        if (!locale) return [ROOT_LOCALE]
 
         List<String> elements = (List<String>) [locale.language,
                                   locale.country, locale.variant].findAll()
@@ -41,6 +45,7 @@ class GroobalizeHelper {
             case 3: locales << locale
             case 2: locales << new Locale(elements[0], elements[1])
             case 1: locales << new Locale(elements[0])
+            case 0: locales << ROOT_LOCALE
         }
         locales
     }
