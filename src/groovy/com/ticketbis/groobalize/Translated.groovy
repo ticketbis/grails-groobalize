c46b9c7110d194ae9552d8952a323579d32725c3
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-10-13 13:13:53 +0200

    Added Translated Trait for Translatable classes

---
+++ src/groovy/com/ticketbis/groobalize/Translated.groovy
@@ -0,0 +1,34 @@
+package com.ticketbis.groobalize
+
+import groovy.transform.CompileStatic
+import groovy.transform.PackageScope
+
+@CompileStatic
+trait Translated {
+    abstract static Class getTranslationClass()
+
+    abstract static List<String> getTranslatedFields()
+
+    abstract Set<Translation> getTranslations()
+
+    // translationsMapCache is lazily computed
+    @PackageScope transient Map<Locale, Translation> translationsMapCache
+
+    Translation getTranslation(Locale locale) {
+        // Use cached version if available
+        if (translationsMapCache)
+            return translationsMapCache[locale]
+
+        getTranslations().find { Translation t -> t.locale == locale }
+    }
+
+    transient Map<Locale, Translation> getTranslationByLocale() {
+        if (!translationsMapCache) {
+            translationsMapCache = new HashMap(getTranslations().size())
+            getTranslations().each { Translation t ->
+                translationsMapCache[t.locale] = t
+            }
+        }
+        translationsMapCache
+    }
+}
