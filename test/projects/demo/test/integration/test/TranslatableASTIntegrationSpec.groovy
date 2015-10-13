615ef391ce85222765d9f84e2193fa7c78c4ed89
Endika Gutiérrez <endika.gutierrez@ticketbis.com>
2015-10-13 13:14:56 +0200

    Added test cases for Translated trait

--- test/projects/demo/test/integration/test/TranslatableASTIntegrationSpec.groovy
+++ test/projects/demo/test/integration/test/TranslatableASTIntegrationSpec.groovy
@@ -2,6 +2,7 @@ package test
 
 import demo.*
 import grails.test.spock.IntegrationSpec
+import com.ticketbis.groobalize.Translated
 
 class TranslatableASTIntegrationSpec extends IntegrationSpec {
 
@@ -17,4 +18,20 @@ class TranslatableASTIntegrationSpec extends IntegrationSpec {
         'title' in Book.translatedFields
         'synopsis' in Book.translatedFields
     }
+
+    void "test Translated trait"() {
+    given:
+        def locale = new Locale('en', 'US')
+        def book = new Book(author: "Dummy author", releaseDate: new Date())
+        def translation = new BookTranslation(title: "title", synopsis: "synopsis", locale: locale)
+        book.addToTranslations(translation)
+    expect:
+        book instanceof Translated
+        translation == book.getTranslation(locale)
+        locale in book.getTranslationByLocale().keySet()
+        translation in book.getTranslationByLocale().values()
+        // now getTranslation uses cached Map
+        translation == book.getTranslation(locale)
+    }
+
 }
